a7681afe0d63be331e2f08f93ad71392d67accc0
Pedro Henrique Maia Alves <parodianos97@gmail.com>
2025-07-09 16:10:24 -0300

    Feat: Criação do Form e de outros componentes. inicialização do Bd em Json Server

---
+++ Costs/src/Components/Projects/ProjectForm.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+import Input from '../Form/Input'
+import Select from '../Form/Select'
+import SubmitButton from '../Form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers:{
+            'Content-type': 'application/json'
+        }
+    }).then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data);
+    })
+    .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <form className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"></Input>
+            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total do projeto"></Input>
+            <div>
+                <Select name="category_id" text="Selecione a categoria" options={categories}/>
+            </div>
+            <SubmitButton text={btnText}/>
+        </form>
+    )
+}
+
+export default ProjectForm
