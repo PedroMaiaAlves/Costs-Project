8a766c42750410a4f5b8f74c75a3a9d8541f9d4b
Pedro Henrique Maia Alves <parodianos97@gmail.com>
2025-07-21 16:34:53 -0300

    Andamento curso react

--- Costs/src/Components/Pages/Projects.jsx
+++ Costs/src/Components/Pages/Projects.jsx
@@ -1,6 +1,69 @@
+import { useState, useEffect } from 'react'
+import {useLocation} from 'react-router-dom'
+
+import Message from "../Layout/Message"
+import styles from './Projects.module.css'
+import Container from '../Layout/Container'
+import LinkButton from '../Layout/LinkButton'
+import ProjectCard from '../Projects/ProjectCard'
+import Loading from '../Layout/Loading'
+
 function Projects(){
+
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
+    useEffect(() => {
+
+        setTimeout(
+            () => {
+                fetch('http://localhost:5000/projects',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((resp) => resp.json())
+        .then((data) =>{
+            console.log(data)
+            setProjects(data)
+            setRemoveLoading(true)
+        })
+        .catch((err) => console.log(err)) 
+            }, 300)
+
+    }, [])
+
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
     return(
-        <h1>Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.tittle_container}>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/NewProject" text="Criar Projeto"/>
+            </div>
+            {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map((project) =>(
+                    <ProjectCard 
+                    id = {project.id}
+                    name={project.name}
+                    budget = {project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    />
+                    ))}
+                    {!removeLoading && <Loading />}
+                    {removeLoading && projects.length == 0 && (
+                     <p>Não há projetos cadastrados</p>   
+                    )}
+            </Container>
+        </div>
     )
 }
 
--- Costs/src/Components/Projects/ProjectForm.jsx
+++ Costs/src/Components/Projects/ProjectForm.jsx
@@ -5,9 +5,10 @@ import Select from '../Form/Select'
 import SubmitButton from '../Form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
+function ProjectForm({ handleSubmit ,btnText, projectData}) {
 
     const [categories, setCategories] = useState([])
+    const[project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -22,12 +23,30 @@ function ProjectForm({btnText}) {
     .catch(err => console.log(err))
     }, [])
 
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e){
+        setProject({ ...project, [e.target.name]: e.target.value})
+    }
+    
+    function handleCategory(e){
+        setProject({ 
+            ...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+        },
+        })
+    }
+
     return (
-        <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"></Input>
-            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total do projeto"></Input>
+        <form onSubmit={submit} className={styles.form}>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" handleOnChange={handleChange} value={project.name ? project.name: ''}></Input>
+            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total do projeto" handleOnChange={handleChange} value={project.budget ? project.budget: ''}></Input>
             <div>
-                <Select name="category_id" text="Selecione a categoria" options={categories}/>
+                <Select name="category_id" text="Selecione a categoria" options={categories} handleOnChange={handleCategory} value={project.category? project.category.id : ''}/>
             </div>
             <SubmitButton text={btnText}/>
         </form>
